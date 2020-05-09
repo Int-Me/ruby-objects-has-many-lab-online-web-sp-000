8bbee39943bfd41a1ad4665dcb9962f8c567f88d
Roland McRae <mcraeroland@gmail.com>
2020-05-09 09:30:25 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+  attr_accessor :name, :author
+
+  def initialize(name)
+    @name = name
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
